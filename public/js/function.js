03e79a90bdc475d40140f8fe7080e2c0d527479d
Gabriel Oliveira <62767640+gabriel4g@users.noreply.github.com>
2020-08-20 20:50:37 -0300

    Add files via upload

---
+++ public/js/function.js
@@ -0,0 +1,8 @@
+function mostrar(name) {
+    document.getElementById(name).style.display = 'inline-block';
+    document.getElementById(name).style.color = 'White';
+}
+
+function ocultar(name) {
+    document.getElementById(name).style.display = 'none';
+}
